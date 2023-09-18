26688b35f2bc83b004490b233bcffafc440ab1c7
Володин Александр <avolodin@htc-cs.ru>
2023-09-18 15:42:12 +0300

    homework-3. Вероятности

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,10 +3,12 @@ import module1.{hof, type_system}
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
 
-    val r: String => Unit = hof.logRunningTime(hof.doomy)
-    r("Doomy")
+    val buckets = module1.homework.collections.createCollection(100000, 3, 3)
+    val results = module1.homework.collections.getResults(buckets)
+    val probability = module1.homework.collections.getProbability(results)
+
+    println(probability)
 
   }
 }
